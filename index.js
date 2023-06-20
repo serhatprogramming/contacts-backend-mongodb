7b33738dacc4af7995e6033b1d3bfc986214aebe
serhatprogramming <serhatprogramming@gmail.com>
2023-06-20 11:26:53 -0400

    validation error added.

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ const errorHandler = (err, req, res, next) => {
   console.log("--------------------------------");
   if (err.name === "CastError") {
     return res.status(400).json({ error: "invalid id" });
+  } else if (err.name === "ValidationError") {
+    return res.status(400).json({ error: err.message });
   }
   next(err);
 };
